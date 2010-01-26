247784f4846d5833ca414714382be0649892bdae
Mark Harrah <dmharrah@gmail.com>
2010-01-25 20:06:34 -0500

    Since ScalaCheck 1.6 is released against 2.7.2-7, build against all of those too!

--- project/build/TestCompatProject.scala
+++ project/build/TestCompatProject.scala
@@ -1,12 +1,11 @@
 import sbt._
 
-class OldTestProject(info: ProjectInfo) extends DefaultProject(info)
+class TestCompatProject(info: ProjectInfo) extends DefaultProject(info)
 {
 	val testInterface = "org.scala-tools.testing" % "test-interface" % "0.4" % "provided"
 	val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5" % "provided"
 	val scalatest = "org.scalatest" % "scalatest" % "1.0" % "provided"
 	val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "provided"
-	override def scratch = true
 
 	// publishing
 	override def managedStyle = ManagedStyle.Maven
